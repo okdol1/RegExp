ef2580bfd7f48098a3fc859f699c47ed33525f1a
okdol1 <okdol0611@gmail.com>
2022-04-01 23:19:45 +0900

    Add README.md

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -118,7 +118,9 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"main.js":[function(require,module,exports) {
-console.log(123);
+var str = "\n010-1234-5678\ntheokdol0611@gmail.com\nhttps://www.omdbapi.com/?apikey=7035c60c&s=frozen\nThe quick brown fox jumps over the lazy dog.\nabbcccdddd\nhttp://localhost:1234/\n\uB3D9\uD574\uBB3C\uACFC_\uBC31\uB450\uC0B0\uC774 \uB9C8\uB974\uACE0 \uB2F3\uB3C4\uB85D\n";
+var h = " the hello world   !\n\n\n";
+console.log(h.replace(/\s/g, ''));
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +149,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60349" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49517" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log(123)
+let str = `
+010-1234-5678
+theokdol0611@gmail.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+http://localhost:1234/
+동해물과_백두산이 마르고 닳도록
+`
+
+const h = ` the hello world   !
+
+
+`
+console.log(h.replace(/\s/g, ''))
