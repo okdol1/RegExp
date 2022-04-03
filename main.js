7e6f77936ef67258f85adbd159e055374008c3d8
okdol1 <okdol0611@gmail.com>
2022-04-04 00:11:59 +0900

    .gitignore 업데이트

--- main.js
+++
@@ -1,9 +0,0 @@
-const str = `
-010-1234-5678
-theokdol0611@gmail.com
-https://www.omdbapi.com/?apikey=7035c60c&s=frozen
-The quick brown fox jumps over the lazy dog.
-abbcccdddd
-`
-
-console.log(str.match(/(?<=@).{1,}/g))
